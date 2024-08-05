0387e5f90ceedac31cbdb461a413f020e850aafc
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-08-04 22:51:57 -0400

    search functionality

--- main.js
+++ main.js
@@ -39,17 +39,19 @@ app.on('activate', () => {
 
 ipcMain.on('search-query', async (event, query) => {
   const apiKey = process.env.API_KEY;
-  const url = `http://www.omdbapi.com/?t=${query}&apikey=${apiKey}`;
+  const url = `http://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
   try {
     const response = await axios.get(url);
     console.log(response.data);
 
+
+    // remove later
     const dataPath = path.join(__dirname, 'data.json');
     fs.writeFileSync(dataPath, JSON.stringify(response.data, null, 2), 'utf-8');
     console.log('Data written to data.json');
 
-    win.loadFile('page.html').then(() => {
-      win.webContents.send('movie-data', response.data);
+    win.loadFile('search.html').then(() => {
+      win.webContents.send('search-data', response.data);
     });
   } catch (error) {
     console.error('Error fetching data:', error);
--- preload.js
+++ preload.js
@@ -2,5 +2,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   sendSearchQuery: (query) => ipcRenderer.send('search-query', query),
-  onMovieData: (callback) => ipcRenderer.on('movie-data', (event, data) => callback(data))
+  onMovieData: (callback) => ipcRenderer.on('movie-data', (event, data) => callback(data)),
+  receive: (channel, func) => {
+    const validChannels = ['search-data'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  }
 });
