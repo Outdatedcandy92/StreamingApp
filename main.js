a0e6fe3f8b0b152d92154018e59ee62a128dec7c
Outdatedcandy92 <138517406+Outdatedcandy92@users.noreply.github.com>
2024-08-05 00:35:44 -0400

    page functionality

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require('path');
 const axios = require('axios');
 const fs = require('fs');
 
-
+let apiKey = process.env.API_KEY;
 let win
 
 function createWindow() {
@@ -17,7 +17,9 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       enableRemoteModule: false,
-    }
+    },
+    icon: path.join(__dirname, 'Artwork/logo-01.png'),
+    autoHideMenuBar: true
   });
 
   win.loadFile('index.html');
@@ -38,7 +40,7 @@ app.on('activate', () => {
 });
 
 ipcMain.on('search-query', async (event, query) => {
-  const apiKey = process.env.API_KEY;
+
   const url = `http://www.omdbapi.com/?s=${query}&apikey=${apiKey}`;
   try {
     const response = await axios.get(url);
@@ -46,12 +48,32 @@ ipcMain.on('search-query', async (event, query) => {
 
 
     // remove later
-    const dataPath = path.join(__dirname, 'data.json');
+    const dataPath = path.join(__dirname, 'logs/search-query.json');
     fs.writeFileSync(dataPath, JSON.stringify(response.data, null, 2), 'utf-8');
     console.log('Data written to data.json');
 
     win.loadFile('search.html').then(() => {
       win.webContents.send('search-data', response.data);
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+});
+
+ipcMain.on('info-query', async (event,query) => {
+  console.log('info-query', query);
+  const url = `http://www.omdbapi.com/?i=${query}&apikey=${apiKey}`;
+  try {
+    const response = await axios.get(url);
+    console.log(response.data);
+
+
+    const dataPath = path.join(__dirname, 'logs/info-query.json');
+    fs.writeFileSync(dataPath, JSON.stringify(response.data, null, 2), 'utf-8');
+    console.log('Data written to data.json');
+
+    win.loadFile('page.html').then(() => {
+      win.webContents.send('movie-data', response.data);
     });
   } catch (error) {
     console.error('Error fetching data:', error);
--- preload.js
+++ preload.js
@@ -3,10 +3,19 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
   sendSearchQuery: (query) => ipcRenderer.send('search-query', query),
   onMovieData: (callback) => ipcRenderer.on('movie-data', (event, data) => callback(data)),
+  sendInfoQuery: (query) => ipcRenderer.send('info-query', query),
   receive: (channel, func) => {
     const validChannels = ['search-data'];
     if (validChannels.includes(channel)) {
       ipcRenderer.on(channel, (event, ...args) => func(...args));
     }
+  },
+  ipcRenderer: {
+    send: (channel, data) => ipcRenderer.send(channel, data),
+    on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args))
+  },
+  remote: {
+    getCurrentWindow: () => remote.getCurrentWindow()
   }
+  
 });
